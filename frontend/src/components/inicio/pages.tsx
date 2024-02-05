1bfa4d0a928269a8546e81466bb7187d0efbca1e
Antonio Larios García de León <antoniolar.developer@gmail.com>
2024-02-05 13:01:01 -0600

    v1

--- frontend/src/components/inicio/pages.tsx
+++ frontend/src/components/inicio/pages.tsx
@@ -5,6 +5,8 @@ import logoBlack from '../../assets/morelia.webp';
 import logoWhite from '../../assets/morelia_white.webp';
 import cityBlack from '../../assets/ciudad.webp';
 import cityWhite from '../../assets/ciudad_white.webp';
+import morelia from '../../assets/brilla.webp'
+import ayuntamiento from '../../assets/ayuntamiento.webp'
 import { useTheme } from '@mui/material';
 
 const ButtonsPages = () => {
@@ -13,8 +15,14 @@ const ButtonsPages = () => {
   const city = theme.palette.mode === 'dark' ? cityWhite : cityBlack;
 
   return (
-    <div className='my-10 lg:p-8 p-4'>
-      <img src={logo} alt="Morelia Ciudad Cultural" className='ml-auto mr-auto mb-20 w-56'/>
+    <div className='my-10 mt-24 lg:p-8 p-4'>
+      <div className='flex md:items-center md:justify-evenly justify-center flex-wrap md:mb-14 mb-10'>
+        <img src={logo} alt="Morelia Ciudad Cultural" className='md:w-56 w-40'/>
+        <div className='flex items-center gap-4 md:mt-0 mt-5'>
+          <img src={ayuntamiento} className='w-32 lg:w-44'/>
+          <img src={morelia} className='w-32 lg:w-44'/>
+        </div>
+      </div>
       <div className='flex justify-evenly gap-8 md:flex-nowrap flex-wrap'>
           <a href='/creativa' >
               <img src={creativa} alt='Morelia Ciudad Creativa' className='shadow-2xl rounded-2xl hover:scale-95 duration-300'/>
--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -3,15 +3,13 @@ import ButtonsPages from '../../components/inicio/pages'
 import CarteleraCultural from '../../components/inicio/cartelera'
 import FooterInicio from '../../components/inicio/footerMain'
 import Secretaria from '../../components/inicio/secretaria'
-import Logos from '../../components/inicio/logos'
 
 const Landing = () => {
   return (
     <div>
       <HeaderInicio />
-      <Logos />
-      <CarteleraCultural />
       <ButtonsPages />
+      <CarteleraCultural />
       <Secretaria />
       <FooterInicio />
     </div>
